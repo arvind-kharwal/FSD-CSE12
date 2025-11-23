806ea0705260462eac459db48980fe0e6f7c5fc5
Arvind Kharwal <karvind629@gmail.com>
2025-11-23 22:46:42 +0530

    23 November 2025

---
+++ JavaScript/null.js
@@ -0,0 +1,9 @@
+var obj = null;
+console.log(obj)
+// console.log(typeof(obj))
+// console.log(null == undefined); // true
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+console.log(Number.MAX_VALUE+Number.MAX_VALUE)
+// console.log(-Number.MIN_VALUE-Number.MIN_VALUE)
+console.log(-Number.MAX_VALUE-Number.MAX_VALUE)
