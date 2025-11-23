17585e5b42b587713bfacbd4ccd6b03705055ca8
Arvind Kharwal <karvind629@gmail.com>
2025-11-23 23:15:54 +0530

    23 November 2025

---
+++ JavaScript/string1.js
@@ -0,0 +1,12 @@
+// s = 'Hello, What\'s up "How are you!"'
+// console.log(s)
+// s1 = "Hello, What's up \"How are you!\"";
+// console.log(s1)
+
+// var str1 = 'JavaScript';
+// str1 = str1+'Programming';
+// console.log(str1);
+
+let s = 'JavaScript';
+s[0] = 'K'
+console.log(s);
