05d7b6c7f21c10adcfb8ed00a5ed86122f401557
Arvind Kharwal <karvind629@gmail.com>
2025-11-23 22:30:11 +0530

    23 November 2025

---
+++ JavaScript/let1.js
@@ -0,0 +1,7 @@
+var x= 10;
+console.log(x)
+console.log(typeof(x))
+var x = 'Hello';
+console.log(x)
+console.log(typeof(x))
+console.log(typeof(f))
---
+++ JavaScript/var1.js
@@ -0,0 +1,6 @@
+var count = 10
+function test(){
+    console.log(count);
+}
+test()
+//console.log(count)
